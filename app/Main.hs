4bc38e1345d21d1e6deb77c249fce44d3ebb8680
Guillaume Pagnoux <gpagnoux@gmail.com>
2022-09-26 18:15:17 +0200

    use lazy Text instead of String

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import qualified Data.Text.Lazy.IO as T
+import Data.Text.Lazy (Text)
 import Control.Monad (when)
-import Control.Monad.State (evalState, execState, State, put)
-import Lib()
+import Control.Monad.State (evalStateT, runStateT, StateT, put, lift)
 import System.Environment (getArgs)
 import System.Exit (ExitCode (ExitFailure), exitWith)
 import System.IO (hFlush, hPutStr, hPutStrLn, isEOF, stderr)
@@ -21,8 +24,8 @@ main = do
 
 runFile :: FilePath -> IO ()
 runFile path = do
-  fileContent <- readFile path
-  let hasError = execState (run fileContent) False
+  fileContent <- T.readFile path
+  (_, hasError) <- runStateT (run fileContent) False
   when hasError $ exitWith (ExitFailure 65)
 
 runPrompt :: IO ()
@@ -32,16 +35,16 @@ runPrompt = do
   if end
     then return ()
     else do
-      line <- getLine
-      evalState (run line) False >> runPrompt
+      line <- T.getLine
+      evalStateT (run line) False >> runPrompt
 
-run :: String -> State InterpreterError (IO ())
-run input = return $ putStrLn $ "Input Program: " ++ input
+run :: Text -> StateT InterpreterError IO ()
+run input = lift $ T.putStrLn $ "Input Program: " <> input
 
-error :: Int -> String -> State InterpreterError (IO ())
-error line message = do
+interpreterError :: Int -> String -> StateT InterpreterError IO ()
+interpreterError line message = do
   put True
-  return $ report ""
+  lift $ report ""
 
   where
     report :: String -> IO ()
